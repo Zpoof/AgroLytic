22b5a19d65e1841d9fb37b9587d08aeb777dfdb4
Zpoof <72591052+Zpoof@users.noreply.github.com>
2021-09-17 11:15:04 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,11 +12,16 @@ from sklearn.tree import DecisionTreeRegressor
 st.image(Image.open('epic.png'))
 tab = st.sidebar.radio("Navigation", ['SoilID','CropChoice'])
 model_path = "SoilNet_93_86.h5"
+
 SoilNet = load_model(model_path)
 CropChoice = joblib.load("CropChoice.joblib.pkl")
+
 classes = {0:"Alluvial Soil:-{ Rice,Wheat,Sugarcane,Maize,Cotton,Soyabean,Jute }",1:"Black Soil:-{ Virginia, Wheat , Jowar,Millets,Linseed,Castor,Sunflower} ",2:"Clay Soil:-{ Rice,Lettuce,Chard,Broccoli,Cabbage,Snap Beans }",3:"Red Soil:{ Cotton,Wheat,Pilses,Millets,OilSeeds,Potatoes }"}
 crops = ["Rice  \nWheat  \nSugarcane  \nMaize  \nCotton  \nSoyabean  \nJute", 
 "Wheat  \nJowar  \nMillets  \nLinseed  \nCastor  \nSunflower", "Rice  \nLettuce  \nChard  \nBroccoli  \nCabbage  \nSnap Beans", "Cotton  \nWheat  \nPulses  \nMillets  \nOil Seeds  \nPotatoes"]
+
+choices = ['wheat','mungbean','Tea','millet','maize','lentil','jute','cofee','cotton','ground nut','peas','rubber','sugarcane','tobacco','kidney beans','moth beans','coconut','blackgram','adzuki beans','pigeon peas','chick peas','banana','grapes','apple','mango','muskmelon','orange','papaya','watermelon','pomegranate']
+
 def model_predict(image,model):
     print("Predicted")
     #image = load_img(image_path,target_size=(224,224))
@@ -65,5 +70,14 @@ elif tab == "CropChoice":
   humid = st.slider("Humidity", min_value = 0.0, max_value =100.0, value = 50.0, step = 0.2)
   ph = st.slider("Ph", min_value = 0.0, max_value = 14.0, value = 5.0, step = 0.1)
   rainfall = st.slider("Annual Rainfall", min_value = 0.0, max_value = 400.0, value = 100.0, step = 1.0)
-  
+  x.append(temp)
+  x.append(humid)
+  x.append(ph)
+  x.append(rainfall)
+  if st.button("Find Crop"):
+    pred = CropChoice.predict([x])
+    pred = pred[1]
+    for n in range(len(pred)):
+      if pred[n] == 1:
+        st.header("Optimal Crop: " + choice[n])
 
